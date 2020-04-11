90dfdbf96c610a15fd1cbc9bb908d1e3010f1bda
EliBoii <elizarski@gmail.com>
2020-04-12 00:24:44 +0300

    Slide

---
+++ src/me/s1mple/matrix/slide/Slide.java
@@ -0,0 +1,12 @@
+package me.s1mple.matrix.slide;
+
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class Slide implements Listener {
+    @EventHandler
+    public void onSneak(PlayerSneakEvent event){
+
+    }
+}
