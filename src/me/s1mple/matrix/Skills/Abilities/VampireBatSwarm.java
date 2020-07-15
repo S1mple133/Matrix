9f861d7b86bf46802ea3a5efc6b734974e2eb759
Lamar Cooley <30600688+CrashCringle12@users.noreply.github.com>
2020-07-15 13:06:53 -0400

    update batswarm

--- src/me/s1mple/matrix/Skills/Abilities/VampireBatSwarm.java
+++ src/me/s1mple/matrix/Skills/Abilities/VampireBatSwarm.java
@@ -3,11 +3,13 @@ package me.s1mple.matrix.Skills.Abilities;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.entity.CreatureSpawnEvent;
+
 import org.skills.abilities.AbilityManager;
 import org.skills.abilities.ActiveAbility;
 import org.skills.api.events.SkillActiveStateChangeEvent;
+import org.skills.api.events.SkillToggleAbilityEvent;
 import org.skills.data.managers.SkilledPlayer;
+import org.skills.types.SkillManager;
 
 import com.clanjhoo.vampire.VampireRevamp;
 import com.clanjhoo.vampire.entity.UPlayer;
@@ -19,26 +21,32 @@ public class VampireBatSwarm extends ActiveAbility {
 		super(skill, name, activateOnReady);
 		AbilityManager.register(this);
 	}
+
+   @EventHandler
+	public void toggleBatSwarm(SkillToggleAbilityEvent event) {
+	   if (event.getAbility() == this) {
+			event.getPlayer().sendRawMessage("§cThis ability can't exactly be toggled yet. Sorry!");
+	   }
+	}
    @EventHandler
 	public void testingThing(SkillActiveStateChangeEvent event) {
 		Player player = event.getPlayer();
-		System.out.println("Testeroni");
-		if (UPlayer.get(player) != null && UPlayer.get(player).isNosferatu()) {
+		if (SkilledPlayer.getSkilledPlayer(player).getSkill() != SkillManager.getSkill("Vampire"))
+		{
+			
+		} else if (UPlayer.get(player) != null && UPlayer.get(player).isNosferatu()) {
 			player.performCommand("v batusi");
-			System.out.println("Success");
-			player.sendRawMessage("§cYou transform!");
 		}
-		else {
-			player.sendRawMessage("§cYou are not a Nosferatu Vampire!");
-		}
-	}
+   }
 	@Override
 	public void useSkill(Player player) {
-		System.out.println("Testeroni");
+	    SkilledPlayer info = activeCheckup(player);
+	    if (info == null) 
+	    	return;
+	    System.out.println("Use Skill successfully triggered");
 		if (UPlayer.get(player) != null && UPlayer.get(player).isNosferatu()) {
 			player.performCommand("v batusi");
-			System.out.println("Success");
-			player.sendRawMessage("§cYou transform!");
+			
 		}
 		else {
 			player.sendRawMessage("§cYou are not a Nosferatu Vampire!");
@@ -47,22 +55,7 @@ public class VampireBatSwarm extends ActiveAbility {
 	public SkilledPlayer activeCheckup(Player arg0) {
 		return SkilledPlayer.getSkilledPlayer(arg0);
 	}
-	@Override
-	public String getActivationKey(SkilledPlayer info) {
-		return "SSL";
-	}
-	@Override
-	public String getTranslatedScaling(SkilledPlayer info, String arg1, Object... arg2) {
-		return "1";
-	}
-	@Override
-	public double getAbsoluteScaling(SkilledPlayer info, String arg1, Object... arg2) {
-		return 1;
-	}
-	@Override
-	public double getCooldown(SkilledPlayer info) {
-		return 1;
-	}
+	
 	@Override
 	public String getTitle(SkilledPlayer info) {
 		return "&4Bat Swarm";
