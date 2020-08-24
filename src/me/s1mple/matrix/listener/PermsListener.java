4b7c91910d2a3697393efd41bd0743fc708e79b1
Darius <pavelescu_darius@yahoo.com>
2020-08-24 17:07:13 +0200

    Switched from PermissionsEx to LuckPerms

--- src/me/s1mple/matrix/listener/PermsListener.java
+++ src/me/s1mple/matrix/listener/PermsListener.java
@@ -1,5 +1,6 @@
 package me.s1mple.matrix.listener;
 
+import me.s1mple.matrix.Matrix;
 import me.s1mple.matrix.Util.Util;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -11,11 +12,6 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
-import ru.tehkode.permissions.PermissionEntity;
-import ru.tehkode.permissions.bukkit.PermissionsEx;
-import ru.tehkode.permissions.events.PermissionEntityEvent;
-import ru.tehkode.permissions.events.PermissionEvent;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,14 +20,14 @@ public class PermsListener implements Listener {
     public static List<Player> playerList = new ArrayList<>();
 
     @EventHandler
-    public void onPex(PermissionEntityEvent event) {
-        if(event.getEntity().has("*") && Bukkit.getServer().getPlayer(event.getEntity().getName()) != null
-        && !event.getEntity().getName().equalsIgnoreCase("CrashCringle12")  && !event.getEntity().getName().equalsIgnoreCase("S1mple133")
-        && !event.getEntity().getName().equalsIgnoreCase("epiclol101") && !event.getEntity().getName().equalsIgnoreCase("TristanReturns")) {
-            event.getEntity().removePermission("*");
-            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ban " + event.getEntity().getName() + " tried giving himself *");
+    public void onPex(PlayerJoinEvent event) {
+        if(event.getPlayer().hasPermission("*")
+        && !event.getPlayer().getName().equalsIgnoreCase("CrashCringle12")  && !event.getPlayer().getName().equalsIgnoreCase("S1mple133")
+        && !event.getPlayer().getName().equalsIgnoreCase("epiclol101") && !event.getPlayer().getName().equalsIgnoreCase("TristanReturns")) {
+            Matrix.getPlugin().getLuckPerms().getUserManager().getUser(event.getPlayer().getUniqueId()).getNodes().clear();
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "ban " + event.getPlayer().getName() + " tried giving himself *");
         }
-        else if((event.getEntity().getName().equalsIgnoreCase("CrashCringle12") && !event.getEntity().getName().equalsIgnoreCase("CrashCringle12"))) {
+        else if(event.getPlayer().getName().equalsIgnoreCase("CrashCringle12")) {
             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "broadcast Nothing but a puff of smoke!");
             Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user CrashCringle12 add *");
 
